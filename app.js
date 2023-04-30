36f75c1b4891995131a733f4961787bdcfcbde94
Will Newton <91620451+WilliamTrueheart@users.noreply.github.com>
2023-04-30 15:42:56 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -71,3 +71,22 @@ function updateMainImage2(num) {
         navItem8.style.opacity = '100%';
     }
 }
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
